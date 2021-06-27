e039eb535541300da2744410b5bc1a6a69822a4a
David Robison <david@robisonweb.dev>
2021-06-27 12:23:30 -0600

    Added shipHull array. Populated shipHull using for loop, set to length. Inside each shipHull position, added object with damge to to false. Updated hit function to check it position watches ship position, and it so change damage to true. returned shipHull.

--- src/components/Ship.js
+++ src/components/Ship.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 const Ship = (name, length) => {
-    const hit = (position) => {
-        console.log(`Position ${position} hit!`);
+    const shipHull = [];
+
+    for (let i = 0; i < length; i++) {
+        shipHull[i] = {
+            damage: false
+        }
     }
-    
+
+    const hit = (position) => {
+        if (position >= 1 && position <= 4) {
+            shipHull[position - 1].damage = true;
+        }
+    }
+
     return {
         name: name,
         length: length,
         hit: hit,
-        sunk: sunk
+        hull: shipHull
     };
 }
 
