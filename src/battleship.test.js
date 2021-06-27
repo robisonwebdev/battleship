55b3565297d9f62b83a09d3dd0e36b39c2ef89fe
David Robison <david@robisonweb.dev>
2021-06-27 12:05:40 -0600

    Added some test for Ship Factory function

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,5 +1,19 @@
 const ship = require('./components/Ship');
 
 test('Setup Test', () => {
-    expect(ship('Patrol Boat', 4, false, false)).toStrictEqual({"name": "Patrol Boat", "hit": false, "length": 4, "sunk": false});
+    let patrolBoat = ship('Patrol Boat', 4);
+
+    expect(patrolBoat.name).toBe('Patrol Boat');
+});
+
+test('Setup Test', () => {
+    let patrolBoat = ship('Patrol Boat', 4);
+
+    expect(patrolBoat.length).toBe(4);
+});
+
+test('Setup Test', () => {
+    let patrolBoat = ship('Patrol Boat', 4);
+
+    expect(patrolBoat.hit(2)).toBe("Position 2 hit!");
 });
