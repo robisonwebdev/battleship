8a7359174eec84cffba7106da5a87bfcfef24b45
David Robison <david@robisonweb.dev>
2021-06-27 12:40:43 -0600

    Tested if ship isSunk function is working

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,19 +1,45 @@
 const ship = require('./components/Ship');
 
-test('Setup Test', () => {
+test('Is the ship sunk?, Yes', () => {
+    let patrolBoat = ship('Patrol Boat', 4);
+    patrolBoat.hit(1)
+    patrolBoat.hit(2)
+    patrolBoat.hit(3)
+    patrolBoat.hit(4)
+
+    expect(patrolBoat.sunk()).toBe(true);
+})
+
+test('Is the ship sunk?, No', () => {
+    let patrolBoat = ship('Patrol Boat', 4);
+    patrolBoat.hit(1)
+    patrolBoat.hit(2)
+    patrolBoat.hit(4)
+
+    expect(patrolBoat.sunk()).toBe(false);
+})
+
+test('Ship Hull', () => {
+    let patrolBoat = ship('Patrol Boat', 4);
+    patrolBoat.hit(2);
+
+    expect(patrolBoat.hull[1]).toStrictEqual({'damage': true});
+})
+
+test('Ship name', () => {
     let patrolBoat = ship('Patrol Boat', 4);
 
     expect(patrolBoat.name).toBe('Patrol Boat');
 });
 
-test('Setup Test', () => {
+test('Ship length', () => {
     let patrolBoat = ship('Patrol Boat', 4);
 
     expect(patrolBoat.length).toBe(4);
 });
 
-test('Setup Test', () => {
-    let patrolBoat = ship('Patrol Boat', 4);
+// test('Ship hit test', () => {
+//     let patrolBoat = ship('Patrol Boat', 4);
 
-    expect(patrolBoat.hit(2)).toBe("Position 2 hit!");
-});
+//     expect(patrolBoat.hit(2)).toBe("Position 2 hit!");
+// });
