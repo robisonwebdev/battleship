64fc52bb5bcda589a27925284d2890ea602d09aa
David Robison <david@robisonweb.dev>
2021-06-28 09:47:19 -0600

    Created buildGameboard function to build gameboard with an object inside each grid

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -2,6 +2,25 @@ import React from 'react';
 
 const Gameboard = () => {
     const gameboard = [];
+    
+    const buildGameboard = () => {
+        for (let i = 0; i < 10; i++) {
+            const row = [];
+    
+            for (let j = 0; j < 10; j++) {
+                row[j] = {
+                    position: `${String.fromCharCode(65 + i)}${j + 1}`
+                }
+            }
+    
+            gameboard[i] = row;
+        }
+    }
+
+    return {
+        build: buildGameboard,
+        gameboard: gameboard
+    }
 }
 
 module.exports = Gameboard;
